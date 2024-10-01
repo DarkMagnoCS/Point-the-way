6a553b0cf16d88d0e06a86af7a6a3f98fc18fb7b
Dark <alejandro.magno.cs@gmail.com>
2024-10-01 11:14:55 -0300

    admin site

--- DB/itineraries/models.py
+++ DB/itineraries/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Itinerary(models.Model):
+    name = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
