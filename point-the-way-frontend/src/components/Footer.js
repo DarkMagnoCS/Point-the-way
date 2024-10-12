5b43c37281cc71bf833dd54c9fa77d8e5a9ec699
Dark <alejandro.magno.cs@gmail.com>
2024-10-11 23:09:32 -0300

    change in footer

--- point-the-way-frontend/src/components/Footer.js
+++ point-the-way-frontend/src/components/Footer.js
@@ -2,23 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Footer = () => {
-    return (
-      <FooterWrapper>
-        <p>© 2024 Point-the-way. All rights reserved.</p>
-        <p>Made by Alejandro Contreras</p>
-      </FooterWrapper>
-    );
-  };
+  return (
+    <FooterContainer>
+      <div>&copy; 2024 All Rights Reserved</div>
+      <FooterLink href="https://www.linkedin.com/in/alejandro-contreras-sanguino" target="_blank" rel="noopener noreferrer">
+        Made by Alejandro Contreras
+      </FooterLink>
+    </FooterContainer>
+  );
+};
 
 export default Footer;
 
-const FooterWrapper = styled.footer`
-  padding: 1rem;
+const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column; /* Stack the elements vertically */
+  justify-content: center; /* Center them vertically */
+  align-items: center; /* Center them horizontally */
+  padding: 10px 20px;
   background-color: #333;
   color: white;
+  font-size: 14px;
   text-align: center;
   position: fixed;
   width: 100%;
   bottom: 0;
   left: 0;
+  height: 50px;
+  
+  @media (max-width: 768px) {
+    padding: 15px 20px;
+    height: auto;
+  }
+`;
+
+const FooterLink = styled.a`
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+  margin-top: 5px; /* Add some space between the two lines */
+  
+  &:hover {
+    text-decoration: underline;
+  }
 `;
